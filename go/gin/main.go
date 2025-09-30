d6519790eadfc6a6ab88ccee97a9a336df286c79
Sebastian Graf <sebastian.graf@konschtanz.de>
2025-09-30 23:07:47 +0200

    Add '/peng' route that responds with 'pung'

--- go/gin/main.go
+++ go/gin/main.go
@@ -18,6 +18,11 @@ func setupRouter() *gin.Engine {
 		c.String(http.StatusOK, "pong")
 	})
 
+	// Peng test
+	r.GET("/peng", func(c *gin.Context) {
+		c.String(http.StatusOK, "pung")
+	})
+
 	// Get user value
 	r.GET("/user/:name", func(c *gin.Context) {
 		user := c.Params.ByName("name")
